d4251be3896e051429b39efe62ff82ff5b32d757
Sanket Buktare <72501836+sanketbuktare@users.noreply.github.com>
2023-05-16 19:22:03 +0530

    Update views.py

--- server/app/views.py
+++ server/app/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import generics
 from .models import *
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate
 
 
 # Create your views here.
